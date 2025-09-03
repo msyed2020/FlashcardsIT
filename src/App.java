7d6957a97955e4f6d3033b8f931f2199341ceeca
msyed2020 <mikailsyed@yahoo.com>
2025-09-02 21:28:06 -0400

    make file

--- src/App.java
+++ src/App.java
@@ -1,7 +1,15 @@
 import java.util.*;
+import java.io.File;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        HashMap<String, String> map = new HashMap<>();
+        Scanner scan = new Scanner(System.in);
+
+    }
+
+    void readFileIT() {
+        File file = new File("italian.txt");
+        Scanner scan = new Scanner(file);
     }
 }
