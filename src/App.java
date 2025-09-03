82f251f47dfee3a5b880082e359db22227d520e6
msyed2020 <mikailsyed@yahoo.com>
2025-09-02 21:35:01 -0400

    lol

--- src/App.java
+++ src/App.java
@@ -6,18 +6,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
         Scanner scan = new Scanner(System.in);
-
+        readFile(map);
+        
     }
 
-    void readFile(HashMap<String, String> map) {
+    static void readFile(HashMap<String, String> map) {
         try {
             File it = new File("italian.txt");
             File en = new File("english.txt");
             Scanner itScan = new Scanner(it);
             Scanner enScan = new Scanner(en);
             while ((itScan.hasNextLine()) && (enScan.hasNextLine())) {
-                String line = itScan.nextLine();
-
+                String itLine = itScan.nextLine();
+                String enLine = enScan.nextLine();
+                map.put(itLine, enLine);
             }
             itScan.close();
             enScan.close();
