4f4587e8f7bec2bd6d66b888252d11665e36631a
msyed2020 <mikailsyed@yahoo.com>
2025-11-09 10:39:20 -0500

    flsh

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-// Here are flashcards right
-
-// Don't even study italian anymore :(
-
-// It was all a fantasy
-
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
