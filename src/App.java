d9f38535707f256188c16c6906fd4d19e3be07cd
msyed2020 <mikailsyed@yahoo.com>
2025-11-13 12:49:01 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 // codependent no more >:)
 
+// nah im done g
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
