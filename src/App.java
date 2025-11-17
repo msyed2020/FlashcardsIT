59febc95e10e5b329a5c0534867dc3c2dd210539
msyed2020 <mikailsyed@yahoo.com>
2025-11-17 09:39:16 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import java.io.*;
 
+// fighting my demons has a good filtered saw wave
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
