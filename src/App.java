5e5f3f1d289066d17194c20744a015ae750ef48f
msyed2020 <mikailsyed@yahoo.com>
2025-11-20 09:53:43 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-// lol im so tired of being here
-
-// and i need to get back to dsp
-
-// codings weird, dsp fine tho
-
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
