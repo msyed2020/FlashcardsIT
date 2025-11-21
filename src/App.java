0d54c65784e2fde8d711a53b436db1ecfc97d3b0
msyed2020 <mikailsyed@yahoo.com>
2025-11-21 08:10:13 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -5,6 +5,8 @@ import java.io.*;
 
 // metal gear solid
 
+// aint no cryin nor self pity
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
