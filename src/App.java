53202aa623a2a8e9bb456e528af37b4f38beb10b
msyed2020 <mikailsyed@yahoo.com>
2025-11-24 16:22:39 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 // lowk too much glaze lol
 
+// kinda done w italian this is dead lol
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
