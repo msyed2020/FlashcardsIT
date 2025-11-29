68b2d166cb0d0db1a3fe03a2a9a3cc9e47c9d50d
msyed2020 <mikailsyed@yahoo.com>
2025-11-29 08:37:08 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -5,6 +5,8 @@ import java.io.*;
 
 // i was more hood b4 i made rap
 
+// drank thru my life
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
