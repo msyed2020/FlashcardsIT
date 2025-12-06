738378442595595486e45a67287bbfe7064e7fd8
msyed2020 <mikailsyed@yahoo.com>
2025-12-06 09:12:18 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-// i aint running after no one lol
-
-// lol whaddup
-
-// im not desperate says the desperate person
-
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
