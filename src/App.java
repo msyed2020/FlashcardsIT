b9328cf7a1312054891e8018443b4c4a4bef7138
msyed2020 <mikailsyed@yahoo.com>
2025-12-16 07:52:15 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -5,6 +5,8 @@ import java.io.*;
 
 // fnu fnu fnu
 
+// without the n
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
