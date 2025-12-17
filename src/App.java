b32b08e3f3fe8cfcc70a36da1ee11bfe50324112
msyed2020 <mikailsyed@yahoo.com>
2025-12-17 15:53:56 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-// screw this bro
-
-// cs is done for lol
-
-// selling plugins n that's it
-
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
