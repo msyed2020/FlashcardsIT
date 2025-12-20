b9ca247bb2caa72cda95749189bd219918491e1a
msyed2020 <mikailsyed@yahoo.com>
2025-12-20 12:41:52 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import java.io.*;
 
+// doormatta
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
