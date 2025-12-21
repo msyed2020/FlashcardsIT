ee7aea116dd8f55ad4912869a26d8278726b7856
msyed2020 <mikailsyed@yahoo.com>
2025-12-21 09:08:41 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-// vertigo dizziness go brrrr
-
-// look how he spins his head no wonder he got vertigo
-
-// dont bring back anything painful
-
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
