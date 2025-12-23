88ca8afbbb4e478da6cb138b31344bd85854c54b
msyed2020 <mikailsyed@yahoo.com>
2025-12-23 10:10:18 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 // durrrr chatgpt
 
+// only into music lol
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
