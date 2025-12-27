ee8cdc9dd648d45025ae63c1280251ea6feed60c
msyed2020 <mikailsyed@yahoo.com>
2025-12-27 10:06:19 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 // theres nothing on earth like the feeling of greed
 
+// it will all be over soon
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
