4281f2c704b13aadcf8c3c653102117ca249658e
msyed2020 <mikailsyed@yahoo.com>
2025-12-28 10:28:52 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -5,6 +5,8 @@ import java.io.*;
 
 // online is not the move
 
+// yoyo wass good
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
