12ab0398a6a13a169ff6c5c8d0393fe40116726d
msyed2020 <mikailsyed@yahoo.com>
2026-01-01 19:59:22 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-// bro
-
-// so tired tbh
-
-// lol cooked
-
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
