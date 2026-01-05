15f1209e5b629dcfc9309b22cfb64edd1aea89ff
msyed2020 <mikailsyed@yahoo.com>
2026-01-05 15:22:22 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 // ghoul
 
+// i had ideas just gotta execute them
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
