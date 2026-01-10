1270f2b33c6ebb436a3f0589dfd43a964b351a3a
msyed2020 <mikailsyed@yahoo.com>
2026-01-10 11:32:47 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 // diamonds
 
+// lol tired
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
