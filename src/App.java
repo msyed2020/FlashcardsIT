695bb016e8553db2c576cb2f6377e1fec4e0a8ab
msyed2020 <mikailsyed@yahoo.com>
2026-01-13 10:59:13 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -1,10 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-// too tired to meet others
-
-// peeps do too much
-
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
