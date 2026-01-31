c16e7feadc5d45294fb3ae006ff5b46c46857e35
msyed2020 <mikailsyed@yahoo.com>
2026-01-31 12:51:26 -0500

    fl

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 // tmi too fast
 
+// oh snap
+
 public class App {
     public static void main(String[] args) throws Exception {
         HashMap<String, String> map = new HashMap<>();
